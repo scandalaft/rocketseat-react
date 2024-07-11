a130d354ad418a06bf31d8b33cf72795b88d6399
Steffany Candalaft <steffany.duran@gtplan.com.br>
2024-07-10 21:40:03 -0300

    teste

--- 03-dt-money/03-dt-money/src/pages/Transactions/components/SearchForm/index.tsx
+++ 03-dt-money/03-dt-money/src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,39 +1,18 @@
 import { MagnifyingGlass } from "phosphor-react";
 import { SearchFormContainer } from "./styles";
-import { useForm } from "react-hook-form";
-import * as z from 'zod';
-import { zodResolver } from "@hookform/resolvers/zod";
 
-
-const searchFormSchema = z.object({
-    query: z.string(),
-})
-
-//vai retornar a tipagem dos campos do nosso formulario
-type SearchFormInputs = z.infer<typeof searchFormSchema>;
 
 export function SearchForm() {
-    const { 
-        register, 
-        handleSubmit,
-        formState: { isSubmitting }
-    } = useForm<SearchFormInputs>({
-        resolver: zodResolver(searchFormSchema),
-    })
-
-    function handleSearchTransactions(data: SearchFormInputs) {
-        console.log(data)
-    }
-
+    
     return (
-        <SearchFormContainer onSubmit={handleSubmit(handleSearchTransactions)}>
+        <SearchFormContainer>
             <input 
                 type="text" 
                 placeholder="Busque por transações"
-                {...register('query')}
+                
             />
 
-            <button type="submit" disabled={isSubmitting}>
+            <button type="submit" >
                 <MagnifyingGlass size={20} />
                 Buscar
             </button>
