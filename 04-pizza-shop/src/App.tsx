33e2f65d88b46a46f42031f95fa2be15690d8d5c
scandalaft <steffanyc.duran@gmail.com>
2024-07-29 23:08:39 -0300

    config

---
+++ 04-pizza-shop/src/App.tsx
@@ -0,0 +1,13 @@
+import { RouterProvider } from "react-router-dom";
+import './global.css'
+import { router } from "./routes";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+
+export function App() {
+  return (
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | Pizza Shop"/>
+      <RouterProvider router={router} />
+    </HelmetProvider>
+  )
+}
